8a1018bfdce31ba46386a94e052aef559a8d54e6
Olawale Mustapha <walemust28@gmail.com>
2024-03-21 15:10:26 +0000

    streamlit string converter

--- hash256.py
+++ hash256.py
@@ -1,7 +1,7 @@
 import hashlib
 import streamlit as st
 
-st.text("this is my first streamlit code")
+
 def hash_cal(input_string):
 
 #Create the Hash object to the data
@@ -14,7 +14,15 @@ def hash_cal(input_string):
     #Get the hexavlaue
     hash_data = sha256_hash.hexdigest()
 
-    print("SHA256 - Hash Value : ", hash_data)
+    st.write("SHA256 - Hash Value : ", hash_data)
 
-hash_cal("Test1")
+#hash_cal("Test1")
 
+st.title("SHA256")
+
+st.text("Convert my string data to hash code")
+text = st.text_area(" ")
+st.button("Convert", on_click=hash_cal(text))
+
+st.balloons()
+print("my hashlib is complete")
